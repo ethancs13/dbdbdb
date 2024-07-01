c6f39a7e7c6aadff46e3cc6b63fd9d364ef7bdf5
BuildTools <unconfigured@null.spigotmc.org>
2024-07-01 12:18:11 -0600

    cors origin updated for railway app

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "dbdbdb-production.up.railway.app"],
     methods: ["POST", "GET", "DELETE"],
     credentials: true,
   })
