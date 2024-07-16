f74f6bdbab249cfadb93111d320b0d1e77dc5322
BuildTools <unconfigured@null.spigotmc.org>
2024-07-16 17:56:53 -0600

    type to TYPE

--- client/src/components/expense-select-row.jsx
+++ client/src/components/expense-select-row.jsx
@@ -23,8 +23,8 @@ const ExpenseSelectRow = ({ index, data, deleteRow, handleChange, expenseTypes }
           onChange={(event) => handleChange(index, event)}
         >
           {expenseTypes.map((expenseType) => (
-            <option key={expenseType.id} value={expenseType.type}>
-              {expenseType.type}
+            <option key={expenseType.id} value={expenseType.TYPE}>
+              {expenseType.TYPE}
             </option>
           ))}
         </select>
