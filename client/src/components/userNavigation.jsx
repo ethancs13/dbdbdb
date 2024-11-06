4208fbaf9a24c1da4d239bb23530ba3d30f30abb
BuildTools <unconfigured@null.spigotmc.org>
2024-11-06 01:44:41 -0700

    css fix for user nav

--- client/src/components/userNavigation.jsx
+++ client/src/components/userNavigation.jsx
@@ -8,27 +8,70 @@ const userNavigation = () => {
   const currentPath = location.pathname;
 
   return (
-    <div className="expense_navigation_wrapper">
-      <div className="expense_navigation_container">
-        <Link to="/" className={currentPath === "/" ? "user-navigation-items active" : "user-navigation-items"}>
-          <button>Summary</button>
-        </Link>
-        <Link to="/general" className={currentPath === "/general" ? "user-navigation-items active" : "user-navigation-items"}>
-          <button>General</button>
-        </Link>
-        <Link to="/food-beverage" className={currentPath === "/food-beverage" ? "user-navigation-items active" : "user-navigation-items"}>
-          <button>Food & Beverage</button>
-        </Link>
-        <Link to="/mileage" className={currentPath === "/mileage" ? "user-navigation-items active" : "user-navigation-items"}>
-          <button>Mileage</button>
-        </Link>
-        <Link to="/itemized-purchases" className={currentPath === "/itemized-purchases" ? "user-navigation-items active" : "user-navigation-items"}>
-          <button>Itemized Purchases</button>
-        </Link>
-        <Link to="/upload-files" className={currentPath === "/upload-files" ? "user-navigation-items active" : "user-navigation-items"}>
-          <button>Upload Files</button>
-        </Link>
-        <Link to="/history" className={currentPath === "/history" ? "user-navigation-items active" : "user-navigation-items"}>
+    <div className="user_navigation_wrapper">
+      <div className="user_navigation_container">
+        <div className="user_navigation_left">
+          <Link
+            to="/general"
+            className={
+              currentPath === "/general" ? "user-navigation-items active" : "user-navigation-items"
+            }
+          >
+            <button>General</button>
+          </Link>
+          <Link
+            to="/food-beverage"
+            className={
+              currentPath === "/food-beverage"
+                ? "user-navigation-items active"
+                : "user-navigation-items"
+            }
+          >
+            <button>Food & Beverage</button>
+          </Link>
+          <Link
+            to="/mileage"
+            className={
+              currentPath === "/mileage" ? "user-navigation-items active" : "user-navigation-items"
+            }
+          >
+            <button>Mileage</button>
+          </Link>
+          <Link
+            to="/itemized-purchases"
+            className={
+              currentPath === "/itemized-purchases"
+                ? "user-navigation-items active"
+                : "user-navigation-items"
+            }
+          >
+            <button>Itemized Purchases</button>
+          </Link>
+          <Link
+            to="/upload-files"
+            className={
+              currentPath === "/upload-files"
+                ? "user-navigation-items active"
+                : "user-navigation-items"
+            }
+          >
+            <button>Upload Files</button>
+          </Link>
+          <Link
+            to="/"
+            className={
+              currentPath === "/" ? "user-navigation-items active" : "user-navigation-items"
+            }
+          >
+            <button>Summary</button>
+          </Link>
+        </div>
+        <Link
+          to="/history"
+          className={
+            currentPath === "/history" ? "user-navigation-items active" : "user-navigation-items"
+          }
+        >
           <button>History</button>
         </Link>
       </div>
